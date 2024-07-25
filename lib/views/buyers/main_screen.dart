ebec4604305b2a00facc1976b4544e7a00f9042b
Lorena2387 <lorenaramirez23@hotmail.com>
2024-07-24 20:00:48 -0500

    implement bottom navigation bar on the main screen and icon images for the bottom navigation bar

--- lib/views/buyers/main_screen.dart
+++ lib/views/buyers/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
@@ -11,9 +12,50 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Main Screen'),
-      ),
+      bottomNavigationBar: BottomNavigationBar(
+          unselectedItemColor: Colors.black,
+          selectedItemColor: Colors.yellow.shade900,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home),
+              label: 'HOME',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/icons/explore.png',
+                width: 20,
+              ),
+              label: 'CATEGORIES',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/icons/shop.png',
+                width: 20,
+              ),
+              label: 'STORE',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/icons/cart.png',
+                width: 20,
+              ),
+              label: 'CART',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/icons/search.png',
+                width: 20,
+              ),
+              label: 'HOME',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/icons/account.png',
+                width: 20,
+              ),
+              label: 'ACCOUNT',
+            ),
+          ]),
     );
   }
 }
